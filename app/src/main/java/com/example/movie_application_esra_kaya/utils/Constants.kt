c84c406c45ead0eb567b8a822ecaf2e763c6231f
eskaya <esra.kaya@naylalabs.com>
2023-03-27 11:52:39 +0300

    Update themes.

--- app/src/main/java/com/example/movie_application_esra_kaya/utils/Constants.kt
+++ app/src/main/java/com/example/movie_application_esra_kaya/utils/Constants.kt
@@ -1,5 +1,6 @@
 package com.example.movie_application_esra_kaya.utils
 
 object Constants {
-    const val BASE_URL = "https://api.themoviedb.org/4/"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val API_KEY = "46d0b7df20b6dca0b123566e3d926b4b"
 }
