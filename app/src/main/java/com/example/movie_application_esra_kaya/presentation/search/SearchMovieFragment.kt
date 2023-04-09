51a844587dd27ee9e6711d9579cde968d1f311b4
Esra Kaya <esra.kaya@naylalabs.com>
2023-04-09 20:51:30 +0300

    Update search page.

--- app/src/main/java/com/example/movie_application_esra_kaya/presentation/search/SearchMovieFragment.kt
+++ app/src/main/java/com/example/movie_application_esra_kaya/presentation/search/SearchMovieFragment.kt
@@ -52,9 +52,11 @@ class SearchMovieFragment : Fragment() {
             override fun afterTextChanged(s: Editable) {}
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                Handler().postDelayed({
-                    viewModel.getSearchList(s.toString())
-                }, 500L)
+                if(s.length >= 2){
+                    Handler().postDelayed({
+                        viewModel.getSearchList(s.toString())
+                    }, 500L)
+                }
             }
         })
     }
