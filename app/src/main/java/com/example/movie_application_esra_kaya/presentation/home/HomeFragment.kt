84bf4c606556998f08bbfb4a550427f1ede7be9a
Esra Kaya <esra.kaya@naylalabs.com>
2023-04-16 16:25:44 +0300

    Create Home Fragment:wip.

---
+++ app/src/main/java/com/example/movie_application_esra_kaya/presentation/home/HomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.movie_application_esra_kaya.presentation.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movie_application_esra_kaya.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
