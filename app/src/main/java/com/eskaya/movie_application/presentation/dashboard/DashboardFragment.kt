797b6fbd0368d35c395ee9b2e5a6e76a224337a6
Esra Kaya <esra.kaya@naylalabs.com>
2023-04-29 17:46:20 +0300

    Update handler function.

--- app/src/main/java/com/eskaya/movie_application/presentation/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/eskaya/movie_application/presentation/dashboard/DashboardFragment.kt
@@ -2,6 +2,7 @@ package com.eskaya.movie_application.presentation.dashboard
 
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -39,7 +40,7 @@ class DashboardFragment : Fragment() {
     private lateinit var imageSliderAdapter: ImageSliderAdapter
     private lateinit var topRatedAdapter: TopRatedAdapter
     private lateinit var upComingAdapter: UpComingMovieAdapter
-    val handler = Handler()
+    val handler = Handler(Looper.getMainLooper())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
