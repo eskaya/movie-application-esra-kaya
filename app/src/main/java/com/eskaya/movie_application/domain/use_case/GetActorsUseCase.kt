d8e65e1b3778e07a2beeb2a6279cb928a34d18ef
Esra Kaya <esra.kaya@naylalabs.com>
2023-04-30 21:37:45 +0300

    Create Actors useCase.

---
+++ app/src/main/java/com/eskaya/movie_application/domain/use_case/GetActorsUseCase.kt
@@ -0,0 +1,29 @@
+package com.eskaya.movie_application.domain.use_case
+
+import com.eskaya.movie_application.data.remote.models.response.ActorsDto
+import com.eskaya.movie_application.domain.repository.MovieRepository
+import com.eskaya.movie_application.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetActorsUseCase @Inject constructor(
+    private val repository: MovieRepository
+) {
+    operator fun invoke(movieId: Int): Flow<Resource<ActorsDto>> = flow {
+        try {
+            emit(Resource.Loading())
+            val actorList = repository.getMovieActors(movieId)
+            emit(Resource.Success(data = actorList))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.message() ?: "An unexpected error occured"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection."))
+        }
+    }
+}
+
+
+
