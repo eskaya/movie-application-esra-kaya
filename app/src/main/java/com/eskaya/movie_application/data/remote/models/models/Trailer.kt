b73899bdcd815b9a57ddef42dd9e7cdc945036a0
Esra Kaya <esra.kaya@naylalabs.com>
2023-05-06 18:24:19 +0300

    Create Trailers model.

---
+++ app/src/main/java/com/eskaya/movie_application/data/remote/models/models/Trailer.kt
@@ -0,0 +1,20 @@
+package com.eskaya.movie_application.data.remote.models.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Trailer(
+    val id: String,
+    @SerializedName("iso_3166_1")
+    val iso31661: String,
+    @SerializedName("iso_639_1")
+    val iso6391: String,
+    val key: String,
+    val name: String,
+    val official: Boolean,
+    @SerializedName("published_at")
+    val publishedAt: String,
+    val site: String,
+    val size: Int,
+    val type: String
+)
