761abdfa2e36da2f53247fac24935f38ffe1f053
eskaya <esra.kaya@bil.omu.edu.tr>
2023-11-29 14:47:21 +0300

    Create Database.

---
+++ app/src/main/java/com/eskaya/movie_application/data/db/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.eskaya.movie_application.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.eskaya.movie_application.data.db.services.MovieDao
+import com.eskaya.movie_application.data.remote.models.models.MovieItem
+
+@Database(entities = [MovieItem::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        private var lock = Any()
+        operator fun invoke(context: Context) = instance ?: synchronized(lock) {
+            instance ?: createDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext, AppDatabase::class.java, "movie_database"
+        ).build()
+    }
+
+}
--- app/src/main/java/com/eskaya/movie_application/data/db/AppRoomDatabase.kt
+++
@@ -1,4 +0,0 @@
-package com.eskaya.movie_application.data.db
-
-class AppRoomDatabase {
-}
